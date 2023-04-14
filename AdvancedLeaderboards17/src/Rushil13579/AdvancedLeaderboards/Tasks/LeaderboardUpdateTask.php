b70dc81f32faa80b9b16758596e9393c9c24302b
FRashkar-C <114340217+FRashkar-pm@users.noreply.github.com>
2023-04-14 19:16:15 +0700

    Update LeaderboardUpdateTask.php

--- AdvancedLeaderboards17/src/Rushil13579/AdvancedLeaderboards/Tasks/LeaderboardUpdateTask.php
+++ AdvancedLeaderboards17/src/Rushil13579/AdvancedLeaderboards/Tasks/LeaderboardUpdateTask.php
@@ -2,25 +2,22 @@
 
 namespace Rushil13579\AdvancedLeaderboards\Tasks;
 
-use pocketmine\{
-    Server,
-    Player
-};
-
+use pocketmine\Server;
+use pocketmine\player\Player;
 use pocketmine\scheduler\Task;
 
 use Rushil13579\AdvancedLeaderboards\Main;
 
 class LeaderboardUpdateTask extends Task {
 
-    private $main;
+    private Main $main;
 
     public function __construct(Main $main){
         $this->main = $main;
     }
 
-    public function onRun($tick){
-        foreach($this->main->getServer()->getLevels() as $level){
+    public function onRun($tick): void{
+        foreach($this->main->getServer()->getWorldManager()->getWorlds() as $level){
             foreach($level->getEntities() as $entity){
                 if($this->main->isALEntity($entity) !== null){
                     $type = $this->main->typeOfALEntity($entity);
