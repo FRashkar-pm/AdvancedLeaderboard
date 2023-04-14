6f71bb4b5648690fad74290441c4de90444a5e74
FRashkar-C <114340217+FRashkar-pm@users.noreply.github.com>
2023-04-14 19:24:07 +0700

    Update XpUpdateTask.php

--- AdvancedLeaderboards17/src/Rushil13579/AdvancedLeaderboards/Tasks/XpUpdateTask.php
+++ AdvancedLeaderboards17/src/Rushil13579/AdvancedLeaderboards/Tasks/XpUpdateTask.php
@@ -2,24 +2,21 @@
 
 namespace Rushil13579\AdvancedLeaderboards\Tasks;
 
-use pocketmine\{
-    Server,
-    Player
-};
-
+use pocketmine\Server;
+use pocketmine\player\Player;
 use pocketmine\scheduler\Task;
 
 use Rushil13579\AdvancedLeaderboards\Main;
 
 class XpUpdateTask extends Task {
 
-    private $main;
+    private Main $main;
 
     public function __construct(Main $main){
         $this->main = $main;
     }
 
-    public function onRun($tick){
+    public function onRun(): void{
         foreach($this->main->getServer()->getOnlinePlayers() as $player){
             $this->main->updateXp($player);
         }
