93d3d5b6785324c65a27aee08cec699f6c65c653
FRashkar-C <114340217+FRashkar-pm@users.noreply.github.com>
2023-04-16 04:53:54 +0700

    Update ALEntity.php

--- AdvancedLeaderboards17/src/Rushil13579/AdvancedLeaderboards/ALEntity.php
+++ AdvancedLeaderboards17/src/Rushil13579/AdvancedLeaderboards/ALEntity.php
@@ -8,6 +8,8 @@ use pocketmine\player\Player;
 use pocketmine\entity\{Skin,Human};
 use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\network\mcpe\protocol\SetActorDataPacket;
+use pocketmine\network\mcpe\protocol\types\entity\EntityMetadataProperties;
+use pocketmine\network\mcpe\protocol\types\entity\EntityMetadataTypes;
 use Rushil13579\AdvancedLeaderboards\Main;
 use Rushil13579\AdvancedLeaderboards\ALEntityTrait;
 
@@ -20,13 +22,23 @@ class ALEntity extends Human {
 
     public function saveNBT(): CompoundTag {
         $nbt = parent::saveNBT();
+        $visibility = 0;
+        if ($this->isNameTagVisible()) {
+            $visibility = 1;
+            if ($this->isNameTagAlwaysVisible()) {
+                $visibility = 2;
+            }
+        }
+        $scale = $this->getNetworkProperties()->getFloat(EntityMetadataProperties::SCALE);
+        $nbt->setInt("NameVisibility", $visibility, true);
+        $nbt->setFloat("Scale", $scale, true);
         return $nbt;
     }
 
     public function sendNameTag(Player $player): void {
         $pk = new SetActorDataPacket();
         $pk->actorRuntimeId = $this->getId();
-        $pk->metadata = [self::NAMETAG => [self::STRING , $this->getDisplayName($player)]];
+        $pk->metadata = [EntityMetadataProperties::NAMETAG => [EntityMetadataTypes::STRING , $this->getDisplayName($player)]];
         $player->getNetworkSession()->sendDataPacket($pk);
     }
 
